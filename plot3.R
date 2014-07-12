2f8e66f58be8ffd09444ec779a68d40276e0c892
Filipe Pais Lenfers <filipelenfers@gmail.com>
2014-07-12 16:54:28 -0300

    Better comments for plot3
    Better comments for plot3.

--- plot3.R
+++ plot3.R
@@ -9,9 +9,13 @@ dates <- as.POSIXct(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S", tz="UTC")
 #open png device
 png("plot3.png", width = 480, height = 480)
 #plot the graph
+##plot sub_metering_1
 plot(dates,as.numeric(as.character(data$Sub_metering_1)),ylab = "Energy sub metering ", xlab=NA,type="l")
+##add sub_metering_2
 lines(dates,as.numeric(as.character(data$Sub_metering_2)), col="red")
+##add sub_metering_3
 lines(dates,as.numeric(as.character(data$Sub_metering_3)), col="blue")
+##add legend
 legend("topright", lty=1, legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black", "red", "blue"))
 #close png device
 dev.off()
