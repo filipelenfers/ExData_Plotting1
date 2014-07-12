360a9664ee62d515165cd2d3bdd291db0ccd719d
Filipe Pais Lenfers <filipelenfers@gmail.com>
2014-07-12 16:51:22 -0300

    Correct weekday language on  Plot 2
    Translate the weekday from portuguese to english.

--- plot2.R
+++ plot2.R
@@ -1,3 +1,5 @@
+#set locale to english, because my machine is in portuguese-Brazil
+Sys.setlocale("LC_TIME", "C")
 #read data
 data <- read.csv2("household_power_consumption.txt", na.strings = "?")
 #select only data between "2007-02-01" and  "2007-02-02"
